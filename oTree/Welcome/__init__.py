76be7cfe64fc8a8b4c53077316be7b4e5946fa3b
crisfigs <c.figueroa@uva.nl>
2025-02-24 12:58:29 +0100

    x

--- oTree/Welcome/__init__.py
+++ oTree/Welcome/__init__.py
@@ -8,6 +8,7 @@ class C(BaseConstants):
     NAME_IN_URL = 'Dana'
     PLAYERS_PER_GROUP = 2
     NUM_ROUNDS = 1
+    TREATMENTS = ['DWK_baseline', 'DWK_cheap', 'DWK_increasedloss', 'DWK_likelyloss']
 
     participation_fee = 2 # in pounds
 
@@ -22,8 +23,8 @@ class Subsession(BaseSubsession):
 
 class Player(BasePlayer):
     prolific_id = models.StringField()
-    treatnumber = models.IntegerField()
     is_human = models.BooleanField(initial=False)
+    assigned_treatment = models.StringField()
 
 def recaptcha_valid(response_token):
     res = requests.post("https://www.google.com/recaptcha/api/siteverify", data={
@@ -32,12 +33,24 @@ def recaptcha_valid(response_token):
     })
     return res.json()["success"]
 
+# Creating session without `self`
+def creating_session(subsession: Subsession):
+    players = subsession.get_players()
+    random.shuffle(players)
+
+    group_matrix = [players[i:i + 2] for i in range(0, len(players), 2)]
+    subsession.set_group_matrix(group_matrix)
+
+    for group in subsession.get_groups():
+        treatment = random.choice(C.TREATMENTS)
+        for p in group.get_players():
+            p.assigned_treatment = treatment  # Only store the string treatment
+
+
 class Welcome(Page):
     form_model = 'player'
     def vars_for_template(player: Player):
-        player.treatnumber = random.choices([1,2,3,4], weights=(1/4,1/4,1/4,1/4), k=1)[0]
         return {
-            'treatnumber': player.treatnumber,
             "RECAPTCHA_SITE_KEY": RECAPTCHA_SITE_KEY
         }
     def live_method(player: Player, data):
@@ -48,18 +61,21 @@ class Welcome(Page):
     def error_message(player, values):
         if not player.is_human:
             return 'You did not solve the captcha.'
-
     @staticmethod
-    def app_after_this_page(player, upcoming_apps):
-        print('upcoming_apps is', upcoming_apps[0],upcoming_apps[1],upcoming_apps[2],upcoming_apps[3])
-        if player.treatnumber == 1:
-            return 'DWK_baseline'
-        elif player.treatnumber == 2:
-            return 'DWK_cheap'
-        elif player.treatnumber == 3:
-            return 'DWK_increasedloss'
-        elif player.treatnumber == 4:
-            return 'DWK_likelyloss'
+    def app_after_this_page(player: Player, upcoming_apps):
+        return player.assigned_treatment  # Redirect to assigned treatment app
+
+    #@staticmethod
+    #def app_after_this_page(player, upcoming_apps):
+     #   print('upcoming_apps is', upcoming_apps[0],upcoming_apps[1],upcoming_apps[2],upcoming_apps[3])
+      #  if player.treatnumber == 1:
+       #     return 'DWK_baseline'
+       # elif player.treatnumber == 2:
+        #    return 'DWK_cheap'
+        #elif player.treatnumber == 3:
+         #   return 'DWK_increasedloss'
+        #elif player.treatnumber == 4:
+         #   return 'DWK_likelyloss'
 
 
 
